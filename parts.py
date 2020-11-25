f74c519c4311c12ce9f1273b5ae28e29b48b5aab
crazy-toy <72115763+crazy-toy@users.noreply.github.com>
2020-11-25 09:05:27 +0800

    Add files via upload

---
+++ parts.py
@@ -0,0 +1,34 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
+)
+from .database import getDatabase
+
+partsbp = Blueprint('parts', __name__, url_prefix='/parts')
+
+@partsbp.route('/')
+def getPartsData():
+    if g.user is None:
+        return redirect(url_for('auth.login'))
+    database=getDatabase()
+    cursor=database.cursor()
+    study='study'
+    techshare='techshare'
+    entertain='entertain'
+    studypart=cursor.execute(
+        'SELECT * FROM post WHERE classification=%s'
+        'ORDER BY updatetime DESC',(study,)
+        ).fetchall()
+    techsharepart=cursor.execute(
+        'SELECT * FROM post WHERE classification=%s'
+        'ORDER BY updatetime DESC',(techshare,)
+        ).fetchall()
+    entertainpart=cursor.execute(
+        'SELECT * FROM post WHERE classification=%s'
+        'ORDER BY updatetime DESC',(entertain,)
+        ).fetchall()
+    return render_template('parts.html', parts={'study':studypart,
+                                                'techshare':techsharepart,
+                                                'entertain':entertainpart})
+
---
+++ posts.py
@@ -0,0 +1,25 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
+)
+
+
+from .database import getDatabase
+
+
+postsbp = Blueprint('posts', __name__, url_prefix='/posts')
+
+@posts.route('/<int::postid>')
+def getPostData(postid):
+    if g.user is None:
+        return redirect(url_for('auth.login'))
+    database=getDatabase()
+    cursor=database.cursor
+    postdata=cursor.execute('SELECT * FROM post WHERE id=%s',(postid)).fetchone()
+    if postdata is None:
+        return render_template('404.html'),404
+    return render_template('posts.html', postdata=postdata)
+
+
+
