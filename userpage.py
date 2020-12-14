9c6b9f65806f7fca281e7b1de2d249c6f96ce5c0
Yaozhtj <1697661410@qq.com>
2020-12-14 15:19:29 +0800

    前后端交接

--- userpage.py
+++ userpage.py
@@ -33,7 +33,7 @@ def showUserpage(id):
     if request.method == 'POST':
         return redirect(url_for('userpage.setting', id=id))
 
-    return render_template('info.html', userpagedata = [user, userinfo, point, level, percentage, needpoint])
+    return render_template('userpage/info.html', userpagedata = [user, userinfo, point, level, percentage, needpoint, id])
 
 
 @userpagebp.route('/<int:id>/collect')
@@ -46,22 +46,24 @@ def collect(id):
         return render_template('404.html'),404
 
     _collect = str(userinfo[3]).split(" ")
+    _collect.remove('')
     _post_id = []
     _posts = []
-    for i in [0, len(_collect)]:
-        _post_id[i] = _collect[i]
+    length=len(_collect)
+    for i in range(0,length):
+        _post_id.append(_collect[i])
 
-    for i in [0, len(_collect)]:
-        _posts[i] = database.execute(
+    for i in range(0,length):
+        cursor.execute(
             'SELECT * FROM post WHERE id=%s'
-            'ORDER BY created DESC;', (_post_id[i], )
-        ).fetchone()
-
+            'ORDER BY posttime DESC;', (_post_id[i], )
+        )
+        _posts.append (cursor.fetchone())
     post = []
     for p in _posts:
         post.append([p[0], p[1], findUser(cursor, p[4])[1], len(p[7].split(' ')) - 1])
 
-    return render_template('collect.html', post = post)
+    return render_template('userpage/collect.html', info = [post, id])
 
 # 展示图片（头像）
 @userpagebp.route('/<int:id>/image')
@@ -74,22 +76,22 @@ def showPosts(id):
     database = getDatabase()
     cursor = database.cursor()
     user = checkUser(cursor, id)
-    userinfo = findUserinfo(cursor, id)
     #error = None
     if user is None:
         return render_template('404.html'),404
 
     # 显示用户所有帖子
-    _posts = database.execute(
+    cursor.execute(
         'SELECT * FROM post WHERE userid=%s'
         'ORDER BY created DESC;', (id, )
-    ).fetchall()
+    )
+    _posts = cursor.fetchall()
 
-    post = []
+    post = [] 
     for p in _posts:
         post.append([p[0], p[1], findUser(cursor, p[4])[1], len(p[7].split(' ')) - 1])
 
-    return render_template('mypost.html', post = post)
+    return render_template('userpage/mypost.html', info = [post, id])
 
 @userpagebp.route('/<int:id>/notice')
 def showNotice(id):
@@ -106,35 +108,38 @@ def showNotice(id):
     _postreply = []
     _post_id = []
     _reply_id = []
-    for i in [0, len(_warn)]:
-        _postreply[i] = _warn[i].split(":")
-        _post_id[i] = _postreply[i][0]          #回帖主题id
-        _reply_id[i] = _postreply[i][1]         #回复id
+    length=len(_warn)
+    for i in range(0,length):
+        _postreply.append(_warn[i].split(":"))
+        _post_id.append( _postreply[i][0])          #回帖主题id
+        _reply_id.append( _postreply[i][1])         #回复id
 
     _posts = []
     _reply = []
-    for i in [0, len(_warn)]:
-        _posts[i] = database.execute(
+    for i in range(0,length):
+        cursor.execute(
             'SELECT * FROM post WHERE id=%s'
             'ORDER BY created DESC;', (_post_id[i], )
-        ).fetchone()
+        )
+        _posts.append( cursor.fetchone())
 
     post = []
     for p in _posts:
         post.append([p[0], p[1], findUser(cursor, p[4])[1], len(p[7].split(' ')) - 1])
 
 
-    for i in [0, len(_warn)]:
-        _reply[i] = database.execute(
+    for i in range(0,length):
+        cursor.execute(
             'SELECT * FROM reply WHERE id=%s'
             'ORDER BY created DESC;', (_reply_id[i], )
-        ).fetchone()
+        )
+        _reply.append(cursor.fetchone())
 
     reply = []
     for r in _reply:
         post.append([r[2]])
 
-    return render_template('notify.html', info = [post, reply])
+    return render_template('userpage/notify.html', info = [post, reply, id])
 
 
 @userpagebp.route('/<int:id>/setting', methods=('GET', 'POST'))
@@ -170,12 +175,13 @@ def setting(id):
             flash(error)
             return render_template('setting.html', userdata={  "oldPassword":oldPassword,
                                                                 "newPassword":newPassword,
-                                                                "newRepassword":newRepassword})
+                                                                "newRepassword":newRepassword,
+                                                                "id":id})
         else:                                                       #修改头像
             file = request.files.get('editormd-image-file')     #获取上传的图片
             uploadImg(id, 'avatar.jpg', file)
 
-    return render_template('setting.html', userdata={})
+    return render_template('userpage/setting.html', userdata={ "id":id })
 
 
 #   以下为功能函数
